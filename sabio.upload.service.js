0b380954fa2d139330f4dd5a750813269975f1eb
varvelac <varvelac@gmail.com>
2016-09-14 10:46:56 -0700

    Add files via upload

---
+++ sabio.upload.service.js
@@ -0,0 +1,38 @@
+﻿if (!sabio.upload) {
+    sabio.upload = { services: {} };
+}
+sabio.upload.services.upload = function (data, onSuccess, onError) {
+
+    var url = "/api/upload/"
+
+    var settings = {
+        data: data,
+        contentType: false,
+        type: "POST",
+        dataType: "json",
+        success: onSuccess,
+        error: onError,
+        cache: false,
+        processData: false,
+    };
+
+    $.ajax(url, settings)
+}
+
+sabio.upload.services.uploadAvatar = function (data, onSuccess, onError) {
+
+    var url = "/api/upload/avatar"
+
+    var settings = {
+        data: data,
+        contentType: false,
+        type: "POST",
+        dataType: "json",
+        success: onSuccess,
+        error: onError,
+        cache: false,
+        processData: false,
+    };
+
+    $.ajax(url, settings)
+}
